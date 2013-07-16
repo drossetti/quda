e06428a304775078b7f2e254015c5b0059194c64
jpfoley <jfoley@physics.utah.edu>
2013-07-16 15:36:11 -0600

    Added reconstruct 9/13 options to ghost-extraction driver

--- lib/extract_gauge_ghost.cu
+++ lib/extract_gauge_ghost.cu
@@ -239,7 +239,13 @@ namespace quda {
       } else if (u.Reconstruct() == QUDA_RECONSTRUCT_8) {
 	extractGhost<Float,length>(FloatNOrder<Float,length,1,8>(u, 0, Ghost), 
 				   u.Nface(), u.SurfaceCB(), u.X(), location);
-      }
+      }else if (u.Reconstruct() == QUDA_RECONSTRUCT_13) {
+	extractGhost<Float,length>(FloatNOrder<Float,length,1,13>(u, 0, Ghost), 
+				   u.Nface(), u.SurfaceCB(), u.X(), location);
+      }else if (u.Reconstruct() == QUDA_RECONSTRUCT_9) {
+	extractGhost<Float,length>(FloatNOrder<Float,length,1,9>(u, 0, Ghost), 
+				   u.Nface(), u.SurfaceCB(), u.X(), location);
+      } 
     } else if (u.Order() == QUDA_FLOAT2_GAUGE_ORDER) {
       if (u.Reconstruct() == QUDA_RECONSTRUCT_NO) {
 	if (typeid(Float)==typeid(short) && u.LinkType() == QUDA_ASQTAD_FAT_LINKS) {
@@ -255,6 +261,12 @@ namespace quda {
       } else if (u.Reconstruct() == QUDA_RECONSTRUCT_8) {
 	extractGhost<Float,length>(FloatNOrder<Float,length,2,8>(u, 0, Ghost), 
 				   u.Nface(), u.SurfaceCB(), u.X(), location);
+      } else if (u.Reconstruct() == QUDA_RECONSTRUCT_13) {
+	extractGhost<Float,length>(FloatNOrder<Float,length,2,13>(u, 0, Ghost),
+				   u.Nface(), u.SurfaceCB(), u.X(), location);
+      } else if (u.Reconstruct() == QUDA_RECONSTRUCT_9) {
+	extractGhost<Float,length>(FloatNOrder<Float,length,2,9>(u, 0, Ghost),
+				   u.Nface(), u.SurfaceCB(), u.X(), location);
       }
     } else if (u.Order() == QUDA_FLOAT4_GAUGE_ORDER) {
       if (u.Reconstruct() == QUDA_RECONSTRUCT_NO) {
@@ -268,8 +280,14 @@ namespace quda {
       } else if (u.Reconstruct() == QUDA_RECONSTRUCT_12) {
 	extractGhost<Float,length>(FloatNOrder<Float,length,4,12>(u, 0, Ghost),
 				   u.Nface(), u.SurfaceCB(), u.X(), location);
-      } else if (u.Reconstruct() == QUDA_RECONSTRUCT_8) {
+      } else if (u.Reconstruct() == QUDA_RECONSTRUCT_8) { 
 	extractGhost<Float,length>(FloatNOrder<Float,length,4,8>(u, 0, Ghost),
+				   u.Nface(), u.SurfaceCB(), u.X(), location);
+      } else if(u.Reconstruct() == QUDA_RECONSTRUCT_13){
+	extractGhost<Float,length>(FloatNOrder<Float,length,4,13>(u, 0, Ghost),
+				   u.Nface(), u.SurfaceCB(), u.X(), location);
+      } else if(u.Reconstruct() == QUDA_RECONSTRUCT_9){
+	extractGhost<Float,length>(FloatNOrder<Float,length,4,9>(u, 0, Ghost),
 				   u.Nface(), u.SurfaceCB(), u.X(), location);
       }
     } else if (u.Order() == QUDA_QDP_GAUGE_ORDER) {
