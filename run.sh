6300c5b2c31c71e0b16ea81d31299dc272b4b944
Davide Rossetti <davide.rossetti@gmail.com>
2016-11-11 13:33:46 -0800

    run is async2 mode

--- run.sh
+++ run.sh
@@ -1,24 +1,36 @@
 #!/bin/bash
 
+source ~/work/gcc-5.3.0.sh
+
 export QUDA_RESOURCE_PATH=$PWD/.quda
 [ ! -d $QUDA_RESOURCE_PATH ] && mkdir -p $QUDA_RESOURCE_PATH
+echo "cleaning performance cache"
+rm -rf $QUDA_RESOURCE_PATH/*
 
+EXE=build/tests/dslash_test
+#EXE=build/tests/dslash_test.plain
+#EXE=build/tests/dslash_test.gpu_comms
 
 function run() {
     local A=$1
     local B=$2
     local C=$3
     local D=$4
-    shift 4
+    local NP=$5
+    local ASYNC=$6
+    local PREPARED=$7
+    shift 7
     local PAR=$@
     date
     (
         echo; echo; \
-        ../scripts/run.sh  -n 2 \
+        ../scripts/run.sh \
+        -n $NP \
         \
         -x QUDA_RESOURCE_PATH=$QUDA_RESOURCE_PATH \
-        -x ASYNC_USE_ASYNC=1 \
-        -x ASYNC_ENABLE_DEBUG=0 \
+        -x QUDA_USE_COMM_ASYNC_STREAM=$ASYNC \
+        -x QUDA_USE_COMM_ASYNC_PREPARED=$PREPARED \
+        -x QUDA_ASYNC_ENABLE_DEBUG=1 \
         \
         -x MP_ENABLE_DEBUG=0 \
         -x GDS_ENABLE_DEBUG=0 \
@@ -40,23 +52,39 @@ function run() {
         -x GDS_ENABLE_WEAK_CONSISTENCY=$C \
         -x GDS_DISABLE_MEMBAR=$D           \
         \
-        ../scripts/wrapper.sh  build/tests/dslash_test $PAR ) 2>&1 | tee -a run.log
+        ../scripts/wrapper.sh  $EXE $PAR ) 
     date
 }
 
-set -x
+#set -x
 
 echo "CWD=$PWD"
 
-L=32
+#exec &>>run.log
 
+#Ni=50
+Ni=20
+#for L in 8 16 32; do
+for L in 16; do
+
+NZ=2
+NT=2
+ASYNC=1
+PREPARED=1
+(
+echo "-------------------------------------------"
+echo "L=$L NZ=$NZ NT=$NT ASYNC=$ASYNC  PREPARED=$PREPARED EXE=$EXE"
+echo "-------------------------------------------"
 # inlcopy seems to be worse if S==1024, better if S==0
-run 0 0 0 0 \
+run 0 0 0 0 $(($NT * $NZ)) $ASYNC $PREPARED \
     --device 0 \
-    --niter 30 --dslash-type wilson \
-    --xdim $L --ydim $L --zdim $L --tdim $(($L / 2)) \
-    --xgridsize 1 --ygridsize 1 --zgridsize 1 --tgridsize 2 \
+    --niter $Ni --dslash-type wilson \
+    --xgridsize 1 --ygridsize 1 --zgridsize $NZ --tgridsize $NT \
+    --xdim $L --ydim $L --zdim $(($L / $NZ)) --tdim $(($L / $NT)) \
     --recon 18 --prec double --kernel-pack-t \
     $@
+) 2>&1 |tee -a run.log
+done
 
-
+#    --xdim $L --ydim $L --zdim $L --tdim $(($L / 2)) \
+#    --xgridsize 1 --ygridsize 1 --zgridsize 1 --tgridsize 2 \
